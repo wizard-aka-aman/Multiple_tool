b0dfc723665c171b2b8b6e80fd3da62a2e97b8d6
Aman Shyam Verma <verma.aman99280@gmail.com>
2025-05-02 15:47:10 +0530

    `Refactored JSON Component Updates: CSS, HTML, and TypeScript Changes`

--- src/app/json/json.component.ts
+++ src/app/json/json.component.ts
@@ -11,10 +11,7 @@ import { ToastrService } from 'ngx-toastr';
 export class JsonComponent {
   text :any ; 
   viwer:any[] =[] ; 
-  constructor(private toastr: ToastrService) {
-    
-    
-  }
+  constructor(private toastr: ToastrService) {}
   onSend(){
     console.log(this.text);
     this.viwer = this.text;
@@ -25,8 +22,8 @@ export class JsonComponent {
     }
     this.viwer = JSON.parse(this.text);
     console.log(this.viwer);
-    
-  }
+    }
+
   copy(){
     this.toastr.success("Text Copied" , "Success");
     navigator.clipboard.writeText(this.text);
